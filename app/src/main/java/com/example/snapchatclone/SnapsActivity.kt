90f4dda07a4adc5253f36284f03a5fed4c8f2cb6
HP <vaibhavsrivastava2107@gmail.com>
2020-03-21 09:58:23 +0530

    Menu initialized

--- app/src/main/java/com/example/snapchatclone/SnapsActivity.kt
+++ app/src/main/java/com/example/snapchatclone/SnapsActivity.kt
@@ -2,11 +2,44 @@ package com.example.snapchatclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SnapsActivity : AppCompatActivity() {
+
+    val mAuth  = FirebaseAuth.getInstance()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item?.itemId == R.id.createSnap) {
+
+        } else if (item?.itemId == R.id.logout) {
+            mAuth.signOut()
+            Toast.makeText(applicationContext,"Logged Out",Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mAuth.signOut()
+        Toast.makeText(applicationContext,"Logged Out",Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snaps)
+
+        setTitle("Snaps")
     }
+
 }
